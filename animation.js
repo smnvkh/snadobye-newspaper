e5dfa6175d007c7aeac13988c765c717550b1c55
smnvkh <s.masha@me.com>
2024-03-28 22:28:16 +0500

    js animation

--- animation.js
+++ animation.js
@@ -114,55 +114,128 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
 
-        // модальное окно для центральной бутылки 
+    // модальное окно для центральной бутылки 
 
-        var modal3 = document.getElementById("third_modal");
-        var btn3 = document.getElementById("right_potion");
-        var span3 = document.getElementsByClassName("close_third")[0];
+      var modal3 = document.getElementById("third_modal");
+      var btn3 = document.getElementById("right_potion");
+      var span3 = document.getElementsByClassName("close_third")[0];
+    
+      btn3.onclick = function() {
+        modal3.style.display = "block";
     
         btn3.onclick = function() {
           modal3.style.display = "block";
-    
-          btn3.onclick = function() {
-            modal3.style.display = "block";
           }
     
-          span3.onclick = function() {
+        span3.onclick = function() {
+          modal3.style.display = "none";
+          }
+    
+        window.onclick = function(event) {
+          if (event.target == modal3) {
             modal3.style.display = "none";
-          }
-    
-          window.onclick = function(event) {
-            if (event.target == modal3) {
-              modal3.style.display = "none";
             }
           }
         }
-})
+      
+      // рандом для гадального шара
+
+      const options = [
+        'да',
+        'нет',
+        'не знаю',
+        'возможно',
+        'определенно',
+        'точно нет',
+        'наверное'
+      ]
+      const magicBall = document.getElementById("mb_container")
+      const response = document.getElementById("response")
+
+      magicBall.addEventListener("click", function() {
+        const index=Math.floor(Math.random()*7);
+        const message = options[index]
+        response.textContent=message;
+        response.style.color='#547629' 
+      }
+      )
+      
+      // алерт для цветов 
+
+      let leftFlower = document.querySelector('.left_flower');
+      leftFlower.addEventListener('click', leftAlert);
+  
+      function leftAlert() {
+        alert("Вы выбрали цветок «Звёздная пыль», мы отправим его вам по магической почте. Доставка займет пять минут, ожидайте!");
+      }
+
+      let centralFlower = document.querySelector('.central_flower');
+      centralFlower.addEventListener('click', centralAlert);
+  
+      function centralAlert() {
+        alert("Вы выбрали цветок «Лунное сияние», мы отправим его вам по магической почте. Доставка займет пять минут, ожидайте!");
+      }
+
+      let rightFlower = document.querySelector('.right_flower');
+      rightFlower.addEventListener('click', rightAlert);
+  
+      function rightAlert() {
+        alert("Вы выбрали цветок «Туманный рассвет», мы отправим его вам по магической почте. Доставка займет пять минут, ожидайте!");
+      }
+
+    // жаба снова в отпуске
+      
+      let frog = document.querySelector('.frog');
+      frog.addEventListener('click', frogAnimation);
+
+      function frogAnimation() {
+        let frog = document.querySelector('.frog');
+        frog.classList.remove('frog');
+        setTimeout(() => {
+          frog.classList.add('frog_animation');
+        }, 0);
+      }
 
     // рука поднимает рюмку 
 
-    function animation() {
       let hand = document.querySelector('.stars_hand');
-      hand.addEventListener('click', animationStart);
-    }
-    
-    function animationStart() {
-      let hand = document.querySelector('.hand');
-      hand.classList.remove('hand');
-      setTimeout(() => {
-        hand.classList.add('handAnimation');
-      }, 0);
-    }
+      hand.addEventListener('click', handAnimation);
 
-    function animationStart() {
-      let hand = document.querySelector('.glass');
-      hand.classList.remove('glass');
+      function handAnimation() {
+        let hand = document.querySelector('.hand');
+        hand.classList.remove('hand');
+        setTimeout(() => {
+          hand.classList.add('handAnimation');
+        }, 0);
+      }
+  
+      function handAnimation() {
+        let hand = document.querySelector('.glass');
+        hand.classList.remove('glass');
+        setTimeout(() => {
+          hand.classList.add('glassAnimation');
+        }, 0);
+      }
+
+    // насекомые вылетают
+    let insects = document.querySelector('.insects');
+    insects.addEventListener('click', insectsAnimation);
+
+    function insectsAnimation() {
+      let butterfly = document.querySelector('.butterfly');
+      let dragonfly = document.querySelector('.dragonfly');
+      let beetle = document.querySelector('.beetle');
+      butterfly.classList.add('butterfly_animation');
+      dragonfly.classList.add('dragonfly_animation');
+      beetle.classList.add('beetle_animation');
       setTimeout(() => {
-        hand.classList.add('glassAnimation');
+        butterfly.classList.remove('butterfly');
+        dragonfly.classList.remove('dragonfly');
+        beetle.classList.remove('beetle');
       }, 0);
     }
+})
+
     
-    document.addEventListener('DOMContentLoaded', () => {
-      animation();
-    });
+
 
